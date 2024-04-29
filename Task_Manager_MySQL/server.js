2e8141cb4b4ae3093787b8c6235ccff27d4ba5a3
Amit Hassan Joy <sarkeramit88@gmail.com>
2024-04-30 00:49:08 +0600

    Password hashing and token generating are added
    Using bcrypt to generate hash pass and jeson web token for giving users a valid token

--- Task_Manager_MySQL/server.js
+++ Task_Manager_MySQL/server.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const connection = require('./database');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken') ;
 const app = express();
 
 app.use(express.json());
+
 
 app.get('/users', (req, res) => {
     const sqlSelect = "SELECT * FROM users";
@@ -11,17 +15,54 @@ app.get('/users', (req, res) => {
     });
 }); //Get all user 
 
-app.post('/users', (req, res) => {
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
-    const role = req.body.role;
-    const sqlInsert = "INSERT INTO users VALUES ('"+username+"','"+email+"','"+password+"' ,'"+`${role}`+"');";
-
-    connection.query(sqlInsert, (err, result) => {
-        res.send(result);
-    });
+app.post('/users',async(req, res) => {
+    const salt = 10;
+    const hashedpass = await bcrypt.hash(req.body.password, salt);
+    try {
+        const username = req.body.username;
+        const email = req.body.email;
+        const role = req.body.role;
+        const sqlInsert = "INSERT INTO users VALUES ('"+username+"','"+email+"','"+hashedpass+"' ,'"+`${role}`+"');";
+        connection.query(sqlInsert, (err, result) => {
+            res.send(result);
+        });} catch (error) {
+        res.status(500).send(error);
+    }
 }); // Create a new user 
+
+
+app.post('/users/login', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const sqlSelect = "SELECT * FROM users WHERE username = '"+username+"';";
+    
+    connection.query(sqlSelect, async (err, result) => {
+        try {
+            if (err) {
+                res.status(500).send(err);
+                return;
+            }
+            if (result.length > 0) {
+                const validpass = await bcrypt.compare(password, result[0].password);                
+                if (validpass) {
+                    const token = jwt.sign({username: username}, process.env.ACCESS_TOKEN_SECRET);
+                    console.log(token);
+                    res.status(201).send({token: token});
+                } else {
+                    res.status(400).send('Invalid Password');
+                }
+            } else {
+                res.status(400).send('Invalid Username');
+                return;
+            }   
+        } catch (error) {
+            res.status(500).send('Request Error');
+        } 
+    }
+    );
+}); // Authentication that generate JWT token which will be used in user authentication
+
+
 
 app.post('/addtask', (req, res) => {
     const id = req.body.id;
